c3d52c1e11766975a52835d3e2a35d3312a65520
Rohit Vyawahare <rohit.vyawahare@synacor.com>
2018-02-03 11:45:45 +0530

    add solution for exercise 3.5

---
+++ thinkpython/3dot5.py
@@ -0,0 +1,37 @@
+def print_pattern1():
+  print '+', '-' * 4, '+', '-' * 4, '+'
+
+def print_pattern2():
+  print '|', ' ' * 4, '|', ' ' * 4, '|'
+
+
+print_pattern1()
+print_pattern2()
+print_pattern2()
+print_pattern2()
+print_pattern2()
+print_pattern1()
+print_pattern2()
+print_pattern2()
+print_pattern2()
+print_pattern2()
+print_pattern1()
+
+
+'''
+OUTPUT:
+
+$ python 3dot5.py 
++ ---- + ---- +
+|      |      |
+|      |      |
+|      |      |
+|      |      |
++ ---- + ---- +
+|      |      |
+|      |      |
+|      |      |
+|      |      |
++ ---- + ---- +
+
+'''
