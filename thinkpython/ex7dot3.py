c7837c2706032975a2c79c15c046165ce3de7614
Rohit Vyawahare <rohit.vyawahare@synacor.com>
2018-02-03 23:10:41 +0530

    solution for exercise 7.3

--- thinkpython/ex7dot3.py
+++ thinkpython/ex7dot3.py
@@ -1,2 +1,9 @@
+import ex7dot2
+import math
 
-import sys
+i = 1.0
+
+while i < 10:
+  print ("> %f %f %f %f"%(i, ex7dot2.square_root(i), math.sqrt(i), abs(ex7dot2.square_root(i) - math.sqrt(i))))
+  i = i + 1
+
