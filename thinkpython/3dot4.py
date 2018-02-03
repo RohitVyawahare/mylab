87eec89d2fd74661edf24d2c8fbcaa9ab961ee69
Rohit Vyawahare <rohit.vyawahare@synacor.com>
2018-02-03 11:38:14 +0530

    add do_four

--- thinkpython/3dot4.py
+++ thinkpython/3dot4.py
@@ -2,20 +2,13 @@ def do_twice(f, s):
   f(s)
   f(s)
 
+def do_four(f,s):
+  do_twice(print_twice, s)
+  do_twice(print_twice, s)
+
 def print_twice(s):
   print s
 
-do_twice(print_twice, 'rohit')
-do_twice(print_twice, 'mrunal')
+do_four(print_twice, 'rohit')
 
-'''
-OUTPUT:
-
-pnq-rvyawahare:thinkpython rohit.vyawahare$ python 3dot4.py 
-rohit
-rohit
-mrunal
-mrunal
-
-'''
 
