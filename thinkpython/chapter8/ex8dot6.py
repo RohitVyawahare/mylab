b3dd1144c41530832e02a14d7d557cea67cff512
Rohit Vyawahare <rohit.vyawahare@synacor.com>
2018-02-04 12:39:19 +0530

    corrections

--- thinkpython/chapter8/ex8dot6.py
+++ thinkpython/chapter8/ex8dot6.py
@@ -1,14 +1,20 @@
 def find(word, key, start):
+  
   count = 0
   index = start
+  
   while index < len(word):
     if word[index] == key:
       count = count + 1
     index += 1
   return count
 
+word = 'rohithihi'
+key = 'h'
+start = 0
 
-print find('rohithihi', 'h', 0)
+print find(word, key, start)
+print word.count('h', 0, len(word) - 2)
 
 '''
 OUTPUT:
